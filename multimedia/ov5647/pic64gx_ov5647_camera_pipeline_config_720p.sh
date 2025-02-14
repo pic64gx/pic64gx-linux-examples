ff749ffb982b601f2cc99a53566511711dca65f5
Shravan Chippa <shravan.chippa@microchip.com>
2025-02-14 15:23:40 +0530

    multimedia: ov5647: updat scripts auto load dt-overlay
    updated demo scripts with auto load capture pipeline
    dt-overlay file and modified log statements
    
    Signed-off-by: Shravan Chippa <shravan.chippa@microchip.com>

--- multimedia/ov5647/pic64gx_ov5647_camera_pipeline_config_720p.sh
+++ multimedia/ov5647/pic64gx_ov5647_camera_pipeline_config_720p.sh
@@ -1,21 +1,36 @@
 #!/bin/sh
 
+cd /boot/ || exit
+
+FILE=pic64gx_curiosity_kit_capture_pipeline_with_ov5647.dtbo
+
+if [ -f $FILE ]; then
+	echo "File $FILE exists."
+	/opt/microchip/dt-overlays/overlay.sh $FILE
+	cd || exit
+	# Wait for the video pipeline enumeration to complete
+	sleep 0.5
+else
+	echo "File $FILE does not exist."
+	exit 1
+fi
+
 capture_media0=$(/usr/bin/media-ctl -p -d /dev/media0 | grep "entity 1:" | awk '{print $4}')
 capture_video0=$(/usr/bin/v4l2-ctl -d /dev/video0  -D | grep "Name" | awk '{print $3}' )
 
 if [ "$capture_video0" = "microchip_vcpp" ]; then
-        echo "found capture device /dev/video0"
         capture_video=/dev/video0
+        echo "Found capture device $capture_video"
 else
-	echo "not found capture device"
+	echo "Capture device /dev/video0 not found"
 	exit 1
 fi
 
 if [ "$capture_media0" = "microchip_vcpp" ]; then
-        echo "found capture media device /dev/media0"
         capture_media=/dev/media0
+        echo "Found capture media device $capture_media"
 else
-	echo "not found capture media device"
+	echo "Capture media device /dev/media0 not found"
 	exit 1
 fi
 
